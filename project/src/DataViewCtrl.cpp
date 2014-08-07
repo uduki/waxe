765205b6166ffd8559f9303ef434b77f66a89873
uduki <cat@oborogin.org>
2014-08-08 04:10:07 +0900

    微修正

--- project/src/DataViewCtrl.cpp
+++ project/src/DataViewCtrl.cpp
@@ -1,14 +1,15 @@
 #include "HaxeAPI.h"
+#include <wx/dataview.h>
 
 value wx_dataviewctrl_create(value inParams, value inValidator)
 {
     wxValidator *validator;
     bool isSuccess = ValueToWX(inValidator, validator);
-    if (!isSuccess || validator == 0) validator = &wxDefaultValidator;
-    CreationParam params(inParams, 0);
+    CreationParams params(inParams, 0);
     wxDataViewCtrl *ctrl = new wxDataViewCtrl(params.parent, params.id,
                                               params.position, params.size,
-                                              params.flags, *validator);
+                                              params.flags,
+                                              (!isSuccess || validator == 0) ? wxDefaultValidator : *validator);
     return WXToValue(ctrl);
 }
 DEFINE_PRIM(wx_dataviewctrl_create,2)
--- wx/DataViewCtrl.hx
+++ wx/DataViewCtrl.hx
@@ -8,17 +8,17 @@ class DataViewCtrl extends Window {
                                    ?inPosition:{ x:Int, y:Int },
                                    ?inSize:{ width:Int, height:Int },
                                    ?inStyle:Int,
-                                   ?inValidator:Void
+                                   ?inValidator:Dynamic
                                    )
     {
-        var params = [ inParent == null ? null : inParent.wxHandle
-                     , inID
-                     , ""
-                     , inPosition
-                     , inSize
-                     , inStyle
-                     ];
-        var handle = wx_dataviewctrl_create(params, inValidator);
+        var handle = wx_dataviewctrl_create(
+            [ inParent == null ? null : inParent.wxHandle
+            , inID
+            , ""
+            , inPosition
+            , inSize
+            , inStyle
+            ], inValidator);
         return new DataViewCtrl(handle);
     }
 
@@ -27,5 +27,5 @@ class DataViewCtrl extends Window {
         super(inHandle);
     }
 
-    static var wx_dataviewctrl_create:Array<Dynamic> -> Dynamic = Loader.load("wx_dataviewctrl_create",2);
+    static var wx_dataviewctrl_create:Array<Dynamic> -> Dynamic -> Dynamic = Loader.load("wx_dataviewctrl_create",2);
 }
